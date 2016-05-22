59f330a50b3d5bc9379e802f269d26c25d9eb7bd
Mathias Klippinge <mathias.klippinge@klarna.com>
2016-05-22 21:07:08 +0200

    Fix bug introduced by fetchWrapper

--- auth-service.js
+++ auth-service.js
@@ -12,18 +12,6 @@ const authKey = 'auth';
 const userKey = 'user';
 
 class AuthService {
-  fetchWrapper(url, headers) {
-    return fetch(url, { headers: headers }).then(response => {
-      if (response.status >= 200 && response.status < 300) {
-        return response
-      }
-      throw {
-        badCredentials: response.status == 401,
-        unknownError: response.status != 401
-      }
-    })
-  }
-
   getAuthInfo(cb) {
     AsyncStorage.multiGet([authKey, userKey], (error, value) => {
       if (error) { return cb(error) }
@@ -48,22 +36,29 @@ class AuthService {
       .Buffer(`${credentials.username}:${credentials.password}`)
       .toString('base64')
 
-    this.fetchWrapper(
-      'https://api.github.com/user',
-      { 'Authorization' : `Basic ${encodedAuth}` }
-    ).then(results => {
-      AsyncStorage.multiSet([
-        [authKey, encodedAuth],
-        [userKey, JSON.stringify(results)]
-      ], error => {
-        if (error) { throw error }
-        return cb({success: true})
+    fetch('https://api.github.com/user', { headers: { 'Authorization' : `Basic ${encodedAuth}` } })
+      .then(response => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json()
+        }
+        throw {
+          badCredentials: response.status == 401,
+          unknownError: response.status != 401
+        }
       })
-    }).catch(error => {
-      return cb(error)
-    }).finally(()=> {
-      return cb({showProgress: false})
-    })
+      .then(results => {
+        AsyncStorage.multiSet([
+          [authKey, encodedAuth],
+          [userKey, JSON.stringify(results)]
+        ], error => {
+          if (error) { throw error }
+          return cb({success: true})
+        })
+      }).catch(error => {
+        return cb(error)
+      }).finally(() => {
+        return cb({showProgress: false})
+      })
   }
 }
 
